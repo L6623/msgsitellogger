7d2e7edf86e27e5cb6346bcfc3ccfa561890dfbc
L6623 <salinasmoralesalex66@gmail.com>
2026-03-05 19:15:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
-import { WebSocketServer } from "ws";
-
-const port = process.env.PORT || 3000;
-
-const wss = new WebSocketServer({ port });
-
-console.log("WebSocket server running on port", port);
-
-wss.on("connection", (ws) => {
-    console.log("Nuevo usuario conectado");
-
-    ws.on("message", (data) => {
-        console.log("Mensaje recibido:", data.toString());
-
-        // Reenviar a todos
-        wss.clients.forEach((client) => {
-            if (client.readyState === 1) {
-                client.send(data.toString());
-            }
-        });
-    });
-
-    ws.on("close", () => {
-        console.log("Usuario desconectado");
-    });
-});
+{
+  "name": "chat-polling",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5"
+  }
+}
